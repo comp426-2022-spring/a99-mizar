fb6478e4ab1066d3e75cfeb158558cc8f2fe402e
uncjackg <jackg@live.unc.edu>
2022-04-27 00:03:37 -0400

    add license, docs folder

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -5,6 +5,8 @@ const Resources = () => {
   return (
     <div id="resources">
       <Nav />
+      <h1 className=''>This page contains a list of mental health resources our team has compiled. </h1>
+      
     </div>
   );
 }
