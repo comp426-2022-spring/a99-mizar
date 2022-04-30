ed95dd3382191c951a858dd3e8c22ad386a85dab
Selina Shi <sshi26@live.unc.edu>
2022-04-30 11:50:03 -0400

    Mental health resources

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -7,12 +7,24 @@ const Resources = () => {
       <Nav />
       <div className='TextContainer'>
         <h1 className='Title'>This page contains a list of mental health resources our team has compiled. </h1>
-        {/* TODO: put resources here in <a> tags that link off of our page
+        {
+          /* TODO: put resources here in <a> tags that link off of our page
             example:
               <a className='' href='RESOURCE_URL'>RESOURCE_NAME</a>
 
             we can style these later, I think it's best to just get them all on the page right now.
-        */}
+        */
+          <a className='' href='https://suicidepreventionlifeline.org/'>National Suicide Prevention Hotline</a>, 
+          <a className='' href='http://www.thehotline.org/'>National Domestic Violence Hotline</a>, 
+          <a className='' href='https://www.veteranscrisisline.net/'>Veterans Crisis Line</a>, 
+          <a className='' href='http://finder.psychiatry.org/'>American Psychiatric Association</a>, 
+          <a className='' href='http://treatment.adaa.org/'>Anxiety and Depression Association of America: Find a Therapist</a>
+          
+        
+        
+        
+        
+          }
       </div>
     </div>
   );
