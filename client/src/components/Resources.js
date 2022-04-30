1ee059f6abb65bc02df192bf6f0796e7f43c9bf4
Selina Shi <sshi26@live.unc.edu>
2022-04-30 11:52:14 -0400

    Mental health resources New

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -18,12 +18,12 @@ const Resources = () => {
           <a className='' href='http://www.thehotline.org/'>National Domestic Violence Hotline</a>, 
           <a className='' href='https://www.veteranscrisisline.net/'>Veterans Crisis Line</a>, 
           <a className='' href='http://finder.psychiatry.org/'>American Psychiatric Association</a>, 
-          <a className='' href='http://treatment.adaa.org/'>Anxiety and Depression Association of America: Find a Therapist</a>
+          <a className='' href='http://treatment.adaa.org/'>Anxiety and Depression Association of America: Find a Therapist</a>, 
+          <a className='' href='https://itunes.apple.com/us/app/breathe2relax/id425720246?mt=8'>Breath2Relax is a free portable stress management tool</a>, 
+          <a className='' href='http://www.allianceofhope.org/'>Alliance of Hope for Suicide Loss Survivors</a>
+
+
           
-        
-        
-        
-        
           }
       </div>
     </div>
