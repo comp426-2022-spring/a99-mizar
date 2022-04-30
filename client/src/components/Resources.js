6f3c58feaa1f0469bdaeab0b44e74384d15dff63
uncjackg <jackg@live.unc.edu>
2022-04-30 12:03:17 -0400

    make resources into a list

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -7,21 +7,29 @@ const Resources = () => {
       <Nav />
       <div className='TextContainer'>
         <h1 className='Title'>This page contains a list of mental health resources our team has compiled. </h1>
-        {
-          /* TODO: put resources here in <a> tags that link off of our page
-            example:
-              <a className='' href='RESOURCE_URL'>RESOURCE_NAME</a>
-
-            we can style these later, I think it's best to just get them all on the page right now.
-        */
-          <a className='' href='https://suicidepreventionlifeline.org/'>National Suicide Prevention Hotline</a>, 
-          <a className='' href='http://www.thehotline.org/'>National Domestic Violence Hotline</a>, 
-          <a className='' href='https://www.veteranscrisisline.net/'>Veterans Crisis Line</a>, 
-          <a className='' href='http://finder.psychiatry.org/'>American Psychiatric Association</a>, 
-          <a className='' href='http://treatment.adaa.org/'>Anxiety and Depression Association of America: Find a Therapist</a>, 
-          <a className='' href='https://itunes.apple.com/us/app/breathe2relax/id425720246?mt=8'>Breath2Relax is a free portable stress management tool</a>, 
-          <a className='' href='http://www.allianceofhope.org/'>Alliance of Hope for Suicide Loss Survivors</a>
-          }
+        <ul>
+          <li>
+            <a className='' href='https://suicidepreventionlifeline.org/'>National Suicide Prevention Hotline</a>
+          </li>
+          <li>
+            <a className='' href='http://www.thehotline.org/'>National Domestic Violence Hotline</a>
+          </li>
+          <li>
+            <a className='' href='https://www.veteranscrisisline.net/'>Veterans Crisis Line</a>
+          </li>
+          <li>
+            <a className='' href='http://finder.psychiatry.org/'>American Psychiatric Association</a>
+          </li>
+          <li>
+            <a className='' href='http://treatment.adaa.org/'>Anxiety and Depression Association of America: Find a Therapist</a>
+          </li>
+          <li>
+            <a className='' href='https://itunes.apple.com/us/app/breathe2relax/id425720246?mt=8'>Breath2Relax is a free portable stress management tool</a>
+          </li>
+          <li>
+            <a className='' href='http://www.allianceofhope.org/'>Alliance of Hope for Suicide Loss Survivors</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
